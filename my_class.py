cad8e6d170eb73b920edf99467d826dee63e8007
ryansba <ryan@servicebureauccelerator.com>
2023-08-14 10:55:28 -0700

    new file:   my_class.py 	new file:   sample.csv

---
+++ my_class.py
@@ -0,0 +1,2 @@
+# Create a class that can be called to fix the formatting of the csv in this dir (sample.csv) and return it as a df. 
+# BONUS: Return the data grouped in the best manner you see fit.
